2f3882e099ca87d05830fa0732be59f039aaddbc
RobertoMoura1 <robertomoura.ads@gmail.com>
2026-04-18 11:43:44 -0300

    feat: update game categories and items

--- src/data/characters.js
+++ src/data/characters.js
@@ -1,90 +1,38 @@
 export const categories = [
-  { id: 'kids', name: '🧸 Personagens' },
-  { id: 'animals', name: '🐶 Animais' },
-  { id: 'objects', name: '🍕 Comida/Objetos' },
-  { id: 'jobs', name: '🎭 Profissões' },
-  { id: 'games', name: '🎮 Jogos' },
-  { id: 'influencers', name: '🎥 YouTubers' },
-  { id: 'creative', name: '🧠 Criativo' },
-  { id: 'brands', name: '🏢 Marcas' },
-  { id: 'apps', name: '📱 Apps/Internet' },
+  { id: 'animals', name: '🐶 ANIMAIS (50)' },
+  { id: 'food', name: '🍕 COMIDAS (50)' },
+  { id: 'objects', name: '🏠 OBJETOS (50)' },
+  { id: 'kids', name: '🎭 PERSONAGENS FÁCEIS (50)' },
 ];
 
 export const characterBank = {
-  kids: [
-    'Peppa Pig', 'George Pig', 'Chase (Patrulha Canina)', 'Marshall', 'Skye', 'Rubble',
-    'Elsa', 'Anna', 'Olaf', 'Moana', 'Maui', 'Rapunzel', 'Cinderela', 'Branca de Neve',
-    'Ariel', 'Bela', 'Aladdin', 'Jasmine', 'Gênio', 'Mickey Mouse', 'Minnie Mouse',
-    'Pato Donald', 'Pateta', 'Simba', 'Nala', 'Woody', 'Buzz Lightyear', 'Jessie',
-    'Barbie', 'Ken', 'Hello Kitty', 'My Melody', 'Kuromi', 'Sonic', 'Tails', 'Mario',
-    'Luigi', 'Princesa Peach', 'Yoshi', 'Pikachu', 'Eevee', 'Ash Ketchum', 'Bob Esponja',
-    'Patrick Estrela', 'Lula Molusco', 'Scooby-Doo', 'Salsicha', 'Tom', 'Jerry',
-    'Ben 10', 'Gumball', 'Darwin', 'Steven Universe', 'Ladybug', 'Cat Noir', 'Bluey', 'Bingo'
+  animals: [
+    'cachorro', 'gato', 'leão', 'tigre', 'elefante', 'girafa', 'zebra', 'macaco', 'urso', 'panda',
+    'cavalo', 'vaca', 'porco', 'galinha', 'pato', 'coelho', 'ovelha', 'cabra', 'lobo', 'raposa',
+    'hiena', 'canguru', 'koala', 'golfinho', 'baleia', 'tubarão', 'polvo', 'peixe', 'tartaruga', 'jacaré',
+    'cobra', 'lagarto', 'camaleão', 'águia', 'coruja', 'papagaio', 'flamingo', 'pinguim', 'foca', 'leão-marinho',
+    'formiga', 'abelha', 'borboleta', 'aranha', 'escorpião', 'minhoca', 'caracol', 'joaninha', 'besouro', 'grilo'
+  ],
+  food: [
+    'pizza', 'hambúrguer', 'batata frita', 'chocolate', 'sorvete', 'bolo', 'brigadeiro', 'pipoca', 'bala', 'chiclete',
+    'maçã', 'banana', 'morango', 'uva', 'melancia', 'laranja', 'pão', 'queijo', 'ovo', 'leite',
+    'refrigerante', 'suco', 'água', 'café', 'chá', 'arroz', 'feijão', 'macarrão', 'lasanha', 'panqueca',
+    'waffle', 'cereal', 'iogurte', 'manteiga', 'margarina', 'mel', 'gelatina', 'pudim', 'doce', 'pirulito',
+    'donut', 'croissant', 'empada', 'coxinha', 'pastel', 'taco', 'sushi', 'hot dog', 'ketchup', 'mostarda'
   ],
   objects: [
-    'Pizza', 'Hambúrguer', 'Batata frita', 'Chocolate', 'Sorvete', 'Bolo', 'Brigadeiro',
-    'Pipoca', 'Bala', 'Chiclete', 'Maçã', 'Banana', 'Morango', 'Uva', 'Melancia',
-    'Laranja', 'Pão', 'Queijo', 'Ovo', 'Leite', 'Refrigerante', 'Suco', 'Água', 'Café',
-    'Chá', 'Prato', 'Copo', 'Garfo', 'Faca', 'Colher', 'Panela', 'Geladeira', 'Fogão',
-    'Microondas', 'Liquidificador', 'Mochila', 'Lápis', 'Caneta', 'Borracha', 'Apontador',
-    'Caderno', 'Livro', 'Estojo', 'Tesoura', 'Régua', 'Cola', 'Celular', 'Tablet',
-    'Computador', 'Controle remoto', 'Televisão', 'Sofá', 'Cama', 'Travesseiro',
-    'Cobertor', 'Toalha', 'Escova de dente', 'Pasta de dente'
+    'mesa', 'cadeira', 'sofá', 'cama', 'travesseiro', 'cobertor', 'toalha', 'espelho', 'porta', 'janela',
+    'geladeira', 'fogão', 'microondas', 'liquidificador', 'panela', 'prato', 'copo', 'garfo', 'faca', 'colher',
+    'televisão', 'controle remoto', 'celular', 'tablet', 'computador', 'laptop', 'carregador', 'lâmpada', 'abajur', 'ventilador',
+    'ar-condicionado', 'escova de dente', 'pasta de dente', 'shampoo', 'sabonete', 'mochila', 'lápis', 'caneta', 'borracha', 'caderno',
+    'livro', 'régua', 'tesoura', 'cola', 'estojo', 'relógio', 'chave', 'cadeado', 'óculos', 'guarda-chuva'
   ],
-  animals: [
-    'Cachorro', 'Gato', 'Leão', 'Tigre', 'Elefante', 'Girafa', 'Zebra', 'Macaco', 'Urso',
-    'Panda', 'Cavalo', 'Vaca', 'Porco', 'Galinha', 'Pato', 'Galo', 'Coelho', 'Ovelha',
-    'Cabra', 'Lobo', 'Raposa', 'Hiena', 'Canguru', 'Koala', 'Golfinho', 'Baleia',
-    'Tubarão', 'Polvo', 'Lula', 'Peixe', 'Tartaruga', 'Jacaré', 'Crocodilo', 'Cobra',
-    'Lagarto', 'Camaleão', 'Águia', 'Coruja', 'Pombo', 'Papagaio', 'Flamingo', 'Pinguim',
-    'Foca', 'Leão-marinho', 'Cavalo-marinho', 'Estrela-do-mar', 'Formiga', 'Abelha',
-    'Borboleta', 'Mosquito', 'Aranha', 'Escorpião', 'Minhoca', 'Caracol', 'Lesma',
-    'Grilo', 'Besouro', 'Joaninha'
-  ],
-  jobs: [
-    'Professor', 'Médico', 'Enfermeiro', 'Dentista', 'Veterinário', 'Policial', 'Bombeiro',
-    'Motorista', 'Piloto', 'Aeromoça', 'Cozinheiro', 'Garçom', 'Padeiro', 'Mecânico',
-    'Engenheiro', 'Arquiteto', 'Advogado', 'Juiz', 'Jornalista', 'Fotógrafo',
-    'Cabeleireiro', 'Manicure', 'Maquiador', 'Ator', 'Cantor', 'Dançarino', 'YouTuber',
-    'Influenciador', 'Gamer', 'Programador', 'Designer', 'Vendedor', 'Caixa',
-    'Segurança', 'Entregador', 'Carteiro', 'Agricultor', 'Pescador', 'Astronauta', 'Cientista'
-  ],
-  creative: [
-    'Algo que voa', 'Algo que nada', 'Algo que corre', 'Algo que brilha', 'Algo que faz barulho',
-    'Algo que é gelado', 'Algo que é quente', 'Algo doce', 'Algo salgado',
-    'Algo que você usa na escola', 'Algo que fica no quarto', 'Algo que fica na cozinha',
-    'Algo que você leva no carro', 'Algo que quebra fácil', 'Algo que é macio',
-    'Algo que é duro', 'Algo redondo', 'Algo quadrado', 'Algo colorido', 'Algo invisível',
-    'Algo que cresce', 'Algo que desaparece', 'Algo que você veste',
-    'Algo que você come todo dia', 'Algo que te dá medo', 'Algo engraçado', 'Algo que pula',
-    'Algo que canta', 'Algo que dança', 'Algo que gira', 'Algo que abre', 'Algo que fecha',
-    'Algo que acende', 'Algo que apaga', 'Algo rápido', 'Algo lento', 'Algo pequeno',
-    'Algo gigante', 'Algo mágico', 'Algo do espaço'
-  ],
-  influencers: [
-    'MrBeast', 'Felipe Neto', 'Luccas Neto', 'Enaldinho', 'Maria Clara e JP', 'Juliana Baltar',
-    'AuthenticGames', 'Rezende', 'Robin Hood Gamer', 'TazerCraft', 'BRKsEDU', 'Nobru',
-    'LOUD Coringa', 'Bibi Tatto', 'Planeta das Gêmeas', 'Camila Loures', 'Virginia Fonseca',
-    'Whindersson Nunes', 'Markiplier', 'PewDiePie', 'SSSniperWolf', 'Like Nastya',
-    'Ryan\'s World', 'Kids Diana Show', 'A for Adley', 'Unspeakable', 'PrestonPlayz',
-    'DanTDM', 'LankyBox', 'Dream'
-  ],
-  games: [
-    'Minecraft', 'Roblox', 'Free Fire', 'Fortnite', 'Among Us', 'Brawl Stars',
-    'Clash Royale', 'Clash of Clans', 'Subway Surfers', 'Temple Run', 'Stumble Guys',
-    'Fall Guys', 'Pokémon GO', 'Mario Kart', 'Super Mario Bros.', 'Sonic the Hedgehog',
-    'The Sims', 'GTA V', 'FIFA', 'eFootball', 'Candy Crush Saga', 'Plants vs Zombies',
-    'Angry Birds', 'Five Nights at Freddy\'s', 'Poppy Playtime', 'Geometry Dash',
-    'Toca Life World', 'PK XD', 'Gacha Life', 'League of Legends'
-  ],
-  brands: [
-    'Nike', 'Adidas', 'Puma', 'McDonald\'s', 'Burger King', 'Coca-Cola', 'Pepsi',
-    'Disney', 'Netflix', 'YouTube', 'TikTok', 'Instagram', 'WhatsApp', 'Google',
-    'Apple', 'Samsung', 'PlayStation', 'Xbox', 'LEGO', 'Hot Wheels', 'Barbie',
-    'Havaianas', 'Nestlé', 'Lacta', 'Oreo'
-  ],
-  apps: [
-    'YouTube', 'TikTok', 'Instagram', 'WhatsApp', 'Spotify', 'Netflix', 'Disney+',
-    'Google Maps', 'Chrome', 'CapCut', 'Canva', 'Zoom', 'Telegram', 'Duolingo', 'Gmail'
+  kids: [
+    'Homem-Aranha', 'Batman', 'Superman', 'Hulk', 'Homem de Ferro', 'Capitão América', 'Thor', 'Elsa', 'Anna', 'Olaf',
+    'Shrek', 'Burro', 'Woody', 'Buzz Lightyear', 'Barbie', 'Ken', 'Mickey Mouse', 'Minnie Mouse', 'Pato Donald', 'Pateta',
+    'Simba', 'Nala', 'Aladdin', 'Jasmine', 'Gênio', 'Mario', 'Luigi', 'Princesa Peach', 'Yoshi', 'Sonic',
+    'Tails', 'Pikachu', 'Ash', 'Bob Esponja', 'Patrick', 'Lula Molusco', 'Scooby-Doo', 'Salsicha', 'Tom', 'Jerry',
+    'Ben 10', 'Gumball', 'Darwin', 'Ladybug', 'Cat Noir', 'Bluey', 'Bingo', 'Peppa Pig', 'George Pig', 'Hello Kitty'
   ]
 };
+
